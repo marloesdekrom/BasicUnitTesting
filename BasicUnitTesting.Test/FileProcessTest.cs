e0f63e0cef268a9930ff8454d695aff4949e48d4
Krom, Marloes de <Marloes.de.Krom@ordina.nl>
2018-10-24 12:57:54 +0200

    modules basic unit testing

---
+++ BasicUnitTesting.Test/FileProcessTest.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Configuration;
+using System.IO;
+
+namespace BasicUnitTesting.Test
+{
+    [TestClass]
+    public class FileProcessTest
+    {
+        private const string BAD_FILE_NAME = @"C:\BadFileName.txt";
+        private string _GoodFileName;
+
+        public TestContext TestContext { get; set; }
+
+        #region Class Initialization and Cleanup
+
+        [ClassInitialize]
+        public static void ClassInitialization(TestContext tc)
+        {
+        }
+
+        [ClassCleanup]
+        public static void ClassCleanUp()
+        {
+        }
+
+        #endregion
+
+        #region Test initialization and cleanup
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            if (TestContext.TestName == "FileNameDoesExist")
+            {
+                SetGoodFileName();
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                   
+                    TestContext.WriteLine("Creating the file " + _GoodFileName);
+                    File.AppendAllText(_GoodFileName, "Blabla");
+
+                }
+            }
+        }
+
+        [TestCleanup]
+        public void TestCleanup()
+        {
+            if (TestContext.TestName == "FileNameDoesExist")
+            {
+                if (!string.IsNullOrEmpty(_GoodFileName))
+                {
+                    TestContext.WriteLine("Deleting the file " + _GoodFileName);
+                    File.Delete(_GoodFileName);
+                }
+            }
+        }
+
+        #endregion
+
+        [TestMethod]
+        public void FileNameDoesExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+           
+            fromCall = fp.FileExists(_GoodFileName);
+           
+
+            Assert.IsTrue(fromCall);
+        }
+
+        [TestMethod]
+        public void FileNameDoesNotExist()
+        {
+            FileProcess fp = new FileProcess();
+            bool fromCall;
+
+            fromCall = fp.FileExists(BAD_FILE_NAME);
+
+            Assert.IsFalse(fromCall);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException()
+        {
+            FileProcess fp = new FileProcess();
+            fp.FileExists("");
+
+        }
+
+
+        [TestMethod]
+        public void FileNameNullOrEmpty_ThrowsArgumentNullException_USeTryCatch()
+        {
+            FileProcess fp = new FileProcess();
+
+            try
+            {
+                fp.FileExists("");
+            }
+            catch (ArgumentNullException)
+            {
+                //Test success
+                return;
+            }
+            Assert.Fail("Call to FileExists did not throw ArgumentNullException");
+        }
+
+        public void SetGoodFileName()
+        {
+            _GoodFileName = ConfigurationManager.AppSettings["GoodFileName"];
+            if (_GoodFileName.Contains("[AppPath]"))
+            {
+                _GoodFileName = _GoodFileName.Replace("[AppPath]",
+                    Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData));
+            }
+        }
+    }
+}
---
+++ BasicUnitTesting.Test/TestInitialization.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace BasicUnitTesting.Test
+{
+    /// <summary>
+    /// Assembly initialize and cleanup methods
+    /// </summary>
+    [TestClass]
+    public class TestInitialization
+    {
+        
+
+        [AssemblyInitialize]
+        public static void AssemblyInitialize(TestContext tc)
+        {
+            tc.WriteLine("In the assembly initialize method");
+            //TODO: Create resources needed for testing
+        }
+
+        [AssemblyCleanup]
+        public static void AssemblyCleanup()
+        {
+            //TODO: Clean up resources used for testing
+        }
+
+    }
+}
